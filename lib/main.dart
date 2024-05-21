31fcde3cdba36abad712617a45a4b9fe8f493b35
Yousuf Uyghur <yousufuighur@yahoo.com>
2024-05-21 05:37:11 +0500

    fixed bugs, added a homepage screen, router to login/register

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,45 @@
+import 'package:dartbasics/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import './views/register_view.dart';
-import './views/login_view.dart';
 // ignore_for_file: avoid_print
 
 void main() {
   WidgetsFlutterBinding
       .ensureInitialized(); // initializes everything at the start
+
   runApp(MaterialApp(
     title: 'Flutter Start',
     theme: ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
       useMaterial3: true,
     ),
-    home: const LoginView(),
+    home: const HomePage(),
   ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          backgroundColor: Colors.greenAccent,
+          foregroundColor: Colors.white,
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return const Text('Done');
+              default:
+                return const Text('Loading....');
+            }
+          },
+        ));
+  }
 }
